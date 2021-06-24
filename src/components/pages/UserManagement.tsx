ed55b46d2739810ea41f4dd7822663550c1865b5
m-abe-dev <66056064+m-abe-dev@users.noreply.github.com>
2021-06-25 04:06:00 +0900

    ユーザー詳細モーダル機能の実装 1, useSelectUserを作成 2, userを読み込み、一致するidをfindで見つけ、変数に代入 3, useSelectUserをUserManagementでimportして、onClickUserでonSelectUserを使って実際のidが一致するのか特定する 4, useSelectUserの中でonOpenできるようにする 5, UserDetailModalでuserの情報を読み込み

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -10,14 +10,21 @@ import {
 import { UserCard } from "../organisms/user/UserCard";
 import { useAllUsers } from "../../hooks/useAllUsers";
 import { UserDetailModal } from "../organisms/modal/UserDetailModal";
+import { useSelectUser } from "../../hooks/useSelectUser";
 
 export const UserManagement: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { getUsers, loading, users } = useAllUsers();
+  const { onSelectUser, selectedUser } = useSelectUser();
 
   useEffect(() => getUsers(), [getUsers]);
 
-  const onClickUser = useCallback(() => onOpen(), [onOpen]);
+  const onClickUser = useCallback(
+    (id: number) => {
+      onSelectUser({ id, users, onOpen });
+    },
+    [users, onSelectUser, onOpen]
+  );
 
   return (
     <>
@@ -27,19 +34,20 @@ export const UserManagement: VFC = memo(() => {
         </Center>
       ) : (
         <Wrap p={{ base: 4, md: 10 }}>
-          {users.map((obj) => (
-            <WrapItem key={obj.id} mx="auto">
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
               <UserCard
+                id={user.id}
                 imageUrl="https://source.unsplash.com/random"
-                userName={obj.username}
-                fullName={obj.name}
+                userName={user.username}
+                fullName={user.name}
                 onClick={onClickUser}
               />
             </WrapItem>
           ))}
         </Wrap>
       )}
-      <UserDetailModal isOpen={isOpen} onClose={onClose} />
+      <UserDetailModal isOpen={isOpen} onClose={onClose} user={selectedUser} />
     </>
   );
 });
